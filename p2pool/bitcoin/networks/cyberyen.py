6097a6367d612bd8733723c50e327aa94aae0d60
raYConnection <georgy@rayconnection.co.uk>
2022-09-06 16:00:40 +0300

    "ltc_scrypt"

--- p2pool/bitcoin/networks/cyberyen.py
+++ p2pool/bitcoin/networks/cyberyen.py
@@ -19,13 +19,13 @@ RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
                           (yield bitcoind.rpc_getblockchaininfo())['chain'] == 'main'
         ))
 SUBSIDY_FUNC = lambda height: 50*100000000 >> (height + 1)//840000
-POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data))
+POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('../../../litecoin_scrypt/build/lib.linux-x86_64-2.7/ltc_scrypt').getPoWHash(data))
 BLOCK_PERIOD = 150 # s
 SYMBOL = 'CY'
 CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Cyberyen') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Cybeyen/') if platform.system() == 'Darwin' else os.path.expanduser('~/.cyberyen'), 'cyberyen.conf')
-BLOCK_EXPLORER_URL_PREFIX = 'https://chainz.cryptoid.info/ltc/block.dws?'
-ADDRESS_EXPLORER_URL_PREFIX = 'https://chainz.cryptoid.info/ltc/address.dws?'
-TX_EXPLORER_URL_PREFIX = 'https://chainz.cryptoid.info/ltc/tx.dws?'
+BLOCK_EXPLORER_URL_PREFIX = 'https://explorer.cyberyen.org/block/'
+ADDRESS_EXPLORER_URL_PREFIX = 'https://explorer.cyberyen.org/address/'
+TX_EXPLORER_URL_PREFIX = 'https://explorer.cyberyen.org/tx/'
 SANE_TARGET_RANGE = (2**256//1000000000000000 - 1, 2**256//1000 - 1)
 DUMB_SCRYPT_DIFF = 2**16
 DUST_THRESHOLD = 0.03e8
