8be1d506adadacb060d47a1301ab1e02863fc6de
raYConnection <georgy@rayconnection.co.uk>
2022-09-06 16:05:11 +0300

    "ltc_scrypt"

--- p2pool/bitcoin/networks/cyberyen.py
+++ p2pool/bitcoin/networks/cyberyen.py
@@ -19,7 +19,7 @@ RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
                           (yield bitcoind.rpc_getblockchaininfo())['chain'] == 'main'
         ))
 SUBSIDY_FUNC = lambda height: 50*100000000 >> (height + 1)//840000
-POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('../../../litecoin_scrypt/build/lib.linux-x86_64-2.7/ltc_scrypt').getPoWHash(data))
+POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data))
 BLOCK_PERIOD = 150 # s
 SYMBOL = 'CY'
 CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Cyberyen') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Cybeyen/') if platform.system() == 'Darwin' else os.path.expanduser('~/.cyberyen'), 'cyberyen.conf')
