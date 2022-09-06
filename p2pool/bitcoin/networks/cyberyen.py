4ac85a2dfdbca7fac83bbd5df670d1f9f026c827
raYConnection <georgy@rayconnection.co.uk>
2022-09-06 14:37:39 +0300

    "correction net cyberyen"

--- p2pool/bitcoin/networks/cyberyen.py
+++ p2pool/bitcoin/networks/cyberyen.py
@@ -11,17 +11,17 @@ P2P_PREFIX = 'c1c1c1c1'.decode('hex')
 P2P_PORT = 58383
 ADDRESS_VERSION = 48
 ADDRESS_P2SH_VERSION = 50
-HUMAN_READABLE_PART = 'ltc'
+HUMAN_READABLE_PART = 'cy'
 RPC_PORT = 58382
 RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
 #            'litecoin' in (yield bitcoind.rpc_help()) and # new versions have "litecoinprivkey" but no "litecoinaddress"
             (yield helper.check_block_header(bitcoind, '34458c96bb547193fa90b2f2599056684b0083d8a2996f2025943eb545031d29')) and
                           (yield bitcoind.rpc_getblockchaininfo())['chain'] == 'main'
         ))
-SUBSIDY_FUNC = lambda height: 50*100000000 >> (height + 1)//840000
+SUBSIDY_FUNC = lambda height: 1000000*100000000 >> (height + 1)//840000
 POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data))
 BLOCK_PERIOD = 150 # s
-SYMBOL = 'LTC'
+SYMBOL = 'CY'
 CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Cyberyen') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Cybeyen/') if platform.system() == 'Darwin' else os.path.expanduser('~/.cyberyen'), 'cyberyen.conf')
 BLOCK_EXPLORER_URL_PREFIX = 'https://chainz.cryptoid.info/ltc/block.dws?'
 ADDRESS_EXPLORER_URL_PREFIX = 'https://chainz.cryptoid.info/ltc/address.dws?'
