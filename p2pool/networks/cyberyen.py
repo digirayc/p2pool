f0d4b43441372411326597b17a5eaabddba8a9a0
raYConnection <georgy@rayconnection.co.uk>
2022-09-07 22:24:35 +0300

    "target correction"

--- p2pool/networks/cyberyen.py
+++ p2pool/networks/cyberyen.py
@@ -2,8 +2,8 @@ from p2pool.bitcoin import networks
 
 PARENT = networks.nets['cyberyen']
 SHARE_PERIOD = 15 # seconds
-CHAIN_LENGTH = 1*60//10 # shares
-REAL_CHAIN_LENGTH = 1*60//10 # shares
+CHAIN_LENGTH = 4*60*60//10 # shares
+REAL_CHAIN_LENGTH = 4*60*60//10 # shares
 TARGET_LOOKBEHIND = 200 # shares
 SPREAD = 3 # blocks
 IDENTIFIER = 'e037d5b8c6923410'.decode('hex')
